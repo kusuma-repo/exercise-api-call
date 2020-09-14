b6bf1ca8684216d5105b9a974b86de9e4b7693bb
faldi kusuma <ifaltiga.pm@gmail.com>
2020-09-14 12:01:02 +0700

    Updates

---
+++ precache-manifest.39acfd63b8c9a3e779101e91469b44a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195d9b62491dab1d4fec5704fb17dc93",
+    "url": "/exercise-api-call/index.html"
+  },
+  {
+    "revision": "03290101f6021735e3d0",
+    "url": "/exercise-api-call/static/css/main.d684dd83.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/exercise-api-call/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exercise-api-call/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03290101f6021735e3d0",
+    "url": "/exercise-api-call/static/js/main.da1ee06b.chunk.js"
+  },
+  {
+    "revision": "c1e4e5b6b1829fe893cc",
+    "url": "/exercise-api-call/static/js/runtime-main.8a9e07e6.js"
+  }
+]);
